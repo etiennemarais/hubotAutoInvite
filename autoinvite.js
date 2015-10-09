11fb7c818eaa44c4e5174a689873cbabccca6e8d
Etienne Marais <etienne.marais@olx.com>
2015-10-09 17:16:01 +0200

    To add lodash as a utility lib, Also finds the channels the given bot identifier is not part of. General cleanup and refactoring. Binds the config to the request callback.

--- autoinvite.js
+++ autoinvite.js
@@ -1,31 +1,39 @@
-(function() {
-
+var _ = require('lodash');
+(function(_) {
     var Invite = function(config, req) {
         this.config = config;
         this.req = req;
     };
 
     Invite.prototype.run = function() {
-        // Get the available channels using the slack token
         var options = {
             url: this.config.slack.url + '/channels.list?token=' + this.config.slack.token
         };
 
-        request(options, processChannels);
+        // Get the available channels using the slack token
+        this.req(options, processChannels.bind(this.config));
     };
 
     var processChannels = function(error, response, responseBody) {
         var data = JSON.parse(responseBody);
-        console.log(data.channels);
+        var _self = this;
 
-        // Get the user ID of jarvis
         // Loop over the channels and see if jarvis is invited to that channel
-        // Auto invite Jarvis to the channel
-        // https://slack.com/api/channels.invite?token=MY_TOKEN&channel=<channelID>&user=<jarvisId>
+        _.forEach(data.channels, function(item) {
+            var channelId = (isBotAMemberOfChannel(item, _self.slack.botId)) ? item.id : '';
+
+            // Auto invite Jarvis to the channel
+            // https://slack.com/api/channels.invite?token=MY_TOKEN&channel=<channelID>&user=<jarvisId>
+            console.log(channelId);
+        });
+    };
+
+    var isBotAMemberOfChannel = function (item, botId) {
+        return (_.indexOf(item.members, botId, true) < 0);
     };
 
     var request = require('request');
     var common = require('./common.js');
     var config = common.config();
     module.exports = new Invite(config, request);
-})();
+})(_);

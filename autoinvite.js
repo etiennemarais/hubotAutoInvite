a19b748dddea0aae559ff193022f3584de4b0990
Etienne Marais <etienne.marais@olx.com>
2015-10-12 09:32:59 +0200

    General - To add comments and a error catch for bad authentication token.

--- autoinvite.js
+++ autoinvite.js
@@ -1,4 +1,6 @@
+// Utility dependencies need to be available everywhere
 var _ = require('lodash');
+
 (function(_) {
     var Invite = function(config, req) {
         this.config = config;
@@ -11,25 +13,29 @@ var _ = require('lodash');
         };
 
         // Get the available channels using the slack token
-        this.req(options, processChannels.bind(this.config));
+        this.req(options, processChannels.bind(this));
     };
 
     var processChannels = function(error, response, responseBody) {
         var data = JSON.parse(responseBody);
         var _self = this;
 
-        // TODO add error reporting for bad auth etc.
+        if (data.ok === false) {
+            console.log("Error: " + data.error);
+            return;
+        }
 
         // Loop over the channels and see if jarvis is invited to that channel
         _.forEach(data.channels, function(item) {
-            var channelId = (isBotAMemberOfChannel(item, _self.slack.botId)) ? item.id : '';
+            var channelId = (isBotAMemberOfChannel(item, _self.config.slack.botId)) ? item.id : '';
 
-            // Auto invite Jarvis to the channel
-            // TODO https://slack.com/api/channels.invite?token=MY_TOKEN&channel=<channelID>&user=<jarvisId>
+            // Auto invite hubot to the channel
+            // TODO https://slack.com/api/channels.invite?token=MY_TOKEN&channel=<channelID>&user=<hubotId>
             console.log(channelId);
         });
     };
 
+    // Checks if bot is a member of the current channel using the channel identifier
     var isBotAMemberOfChannel = function (item, botId) {
         return (_.indexOf(item.members, botId, true) < 0);
     };
